29e44ee55e1672209d761efc8fb7709668de4d6f
Shon Rakesh Gaikwad <48610486+hackersnake@users.noreply.github.com>
2024-06-05 18:08:56 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://shongaikwad10169:shon10169@cluster0.dhupsxb.mongodb.net/project').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect('url').then(()=>console.log("DataBase Connected"));
 }
